33fcd12f67df3e4e85808500921ff8ba18f82a3f
paldeflex <paldeflex@gmail.com>
2024-04-21 16:26:31 +0500

    Небольшие правки

--- new_todo/todoapp/forms.py
+++ new_todo/todoapp/forms.py
@@ -3,7 +3,7 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={"class":"myinput", "placeholder":"Enter Todo"}))
+    title = forms.CharField(widget=forms.TextInput(attrs={"class":"myinput", "placeholder":"Напишите задачу"}))
     class Meta:
         model = Task
         fields = ["title"]
